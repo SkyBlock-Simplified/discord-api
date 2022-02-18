26364cf5cf08b7fec4de3357cce149de251de6b2
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-17 20:20:22 -0500

    PrefixCommand doesn't load, so don't inherit from DiscordObject

--- src/main/java/dev/sbs/discordapi/command/PrefixCommand.java
+++ src/main/java/dev/sbs/discordapi/command/PrefixCommand.java
@@ -1,13 +1,7 @@
 package dev.sbs.discordapi.command;
 
-import dev.sbs.discordapi.DiscordBot;
 import dev.sbs.discordapi.command.data.CommandData;
-import dev.sbs.discordapi.util.DiscordObject;
 
-public class PrefixCommand extends DiscordObject implements CommandData {
-
-    protected PrefixCommand(DiscordBot discordBot) {
-        super(discordBot);
-    }
+public class PrefixCommand implements CommandData {
 
 }
