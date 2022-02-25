903f3e21c3c7c20212fd881b1d1f87c6b02be536
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-25 18:40:53 -0500

    Create and extend from CustomDate class

---
+++ src/main/java/dev/sbs/discordapi/util/DiscordDate.java
@@ -0,0 +1,11 @@
+package dev.sbs.discordapi.util;
+
+import dev.sbs.api.util.date.RealDate;
+
+public class DiscordDate extends RealDate {
+
+    public DiscordDate(long realTime) {
+        super(realTime);
+    }
+
+}
