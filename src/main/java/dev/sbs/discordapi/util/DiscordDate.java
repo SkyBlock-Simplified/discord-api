ac58873bca329914143183354596448f1a101ea5
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-25 20:11:54 -0500

    Complete discord date formatter

--- src/main/java/dev/sbs/discordapi/util/DiscordDate.java
+++ src/main/java/dev/sbs/discordapi/util/DiscordDate.java
@@ -1,15 +1,93 @@
 package dev.sbs.discordapi.util;
 
 import dev.sbs.api.util.date.RealDate;
+import dev.sbs.api.util.helper.FormatUtil;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Optional;
 
 public class DiscordDate extends RealDate {
 
-    public DiscordDate(String duration) {
+    public DiscordDate(@NotNull String duration) {
         super(duration);
     }
 
     public DiscordDate(long realTime) {
         super(realTime);
+    }
+
+    public String asFormat(@NotNull Type type) {
+        return type.asFormat(this.getRealTime());
+    }
+
+    /**
+     * Format Type
+     * <br><br>
+     * All sample dates show Month before Day.
+     */
+    public enum Type {
+
+        /**
+         * MM/dd/yyyy
+         */
+        DATE("d", "MM/dd/yyyy"),
+        /**
+         * MMMM d, yyyy
+         */
+        FULL_DATE("D", "MMMM d, yyyy"),
+        /**
+         * h:m a
+         */
+        TIME("t", "h:m a"),
+        /**
+         * h:m:s a
+         */
+        FULL_TIME("T", "h:m:s a"),
+        /**
+         * MMMM d, yyyy h:m a
+         */
+        FULL_DATE_TIME("f", "MMMM d, yyyy h:m a"),
+        /**
+         * EEEE, MMMM d, yyyy h:m a
+         */
+        WEEKDAY_FULL_DATE_TIME("F", "EEEE, MMMM d, yyyy h:m a"),
+        /**
+         * Shows the date and time relative to {@link Instant#now()}.
+         */
+        RELATIVE("R"),
+        /**
+         * The current date and time in milliseconds.
+         */
+        TIMESTAMP(Optional.empty());
+
+        @Getter private final Optional<String> format;
+        @Getter private final Optional<SimpleDateFormat> dateFormat;
+
+        Type(@NotNull String format) {
+            this(Optional.of(format));
+        }
+
+        Type(@NotNull Optional<String> format) {
+            this(format, Optional.empty());
+        }
+
+        Type(@NotNull String format, @Nullable String dateFormat) {
+            this(Optional.of(format), Optional.ofNullable(dateFormat));
+        }
+
+        Type(@NotNull Optional<String> format, @NotNull Optional<String> dateFormat) {
+            this.format = format;
+            this.dateFormat = dateFormat.map(SimpleDateFormat::new);
+        }
+
+        public String asFormat(long milliseconds) {
+            return this.getFormat().map(format -> FormatUtil.format("<t:{0,number,#}:{1}>", milliseconds, format)).orElse(String.valueOf(milliseconds));
+        }
+
     }
 
 }
