977f68ff1aa65a8223fee1ddffb0f9040c9c588c
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-03-23 21:38:53 -0400

    Add equals/hashCode

--- src/main/java/dev/sbs/discordapi/response/component/layout/LayoutComponent.java
+++ src/main/java/dev/sbs/discordapi/response/component/layout/LayoutComponent.java
@@ -1,5 +1,7 @@
 package dev.sbs.discordapi.response.component.layout;
 
+import dev.sbs.api.util.builder.EqualsBuilder;
+import dev.sbs.api.util.builder.hashcode.HashCodeBuilder;
 import dev.sbs.api.util.collection.concurrent.Concurrent;
 import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.discordapi.response.component.Component;
@@ -12,6 +14,25 @@ public abstract class LayoutComponent<T extends ActionComponent<?>> extends Comp
     private final ConcurrentList<T> components = Concurrent.newList();
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LayoutComponent<?> that = (LayoutComponent<?>) o;
+
+        return new EqualsBuilder()
+            .append(this.getComponents(), that.getComponents())
+            .build();
+    }
+
+    @Override
     public abstract discord4j.core.object.component.LayoutComponent getD4jComponent();
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(this.getComponents())
+            .build();
+    }
 
 }
