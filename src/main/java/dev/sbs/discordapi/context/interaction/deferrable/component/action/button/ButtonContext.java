ef57ed37b7438b7794783c4804f26115b443511f
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-12-17 18:04:45 -0500

    Begin presentModal code

--- src/main/java/dev/sbs/discordapi/context/interaction/deferrable/component/action/button/ButtonContext.java
+++ src/main/java/dev/sbs/discordapi/context/interaction/deferrable/component/action/button/ButtonContext.java
@@ -3,8 +3,11 @@ package dev.sbs.discordapi.context.interaction.deferrable.component.action.butto
 import dev.sbs.discordapi.DiscordBot;
 import dev.sbs.discordapi.context.interaction.deferrable.component.action.ActionComponentContext;
 import dev.sbs.discordapi.response.Response;
+import dev.sbs.discordapi.response.component.interaction.Modal;
 import dev.sbs.discordapi.response.component.interaction.action.Button;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
@@ -18,6 +21,11 @@ public interface ButtonContext extends ActionComponentContext {
 
     default void modify(Function<Button.ButtonBuilder, Button.ButtonBuilder> buttonBuilder) {
         this.modify(buttonBuilder.apply(this.getComponent().mutate()).build());
+    }
+
+    default Mono<Void> presentModal(@NotNull Modal modal) {
+        // TODO: Cache modal associated to response
+        return this.getEvent().presentModal(modal.getD4jPresentSpec());
     }
 
     static ButtonContext of(DiscordBot discordBot, ButtonInteractionEvent event, Response response, Button button) {
