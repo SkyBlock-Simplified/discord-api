317c07746ee5f044ff1e1500925603a1ad74549a
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-01-15 19:41:05 -0500

    Generalized #apply and simplified the existing code into #handleEvent

--- src/main/java/dev/sbs/discordapi/listener/message/component/ComponentListener.java
+++ src/main/java/dev/sbs/discordapi/listener/message/component/ComponentListener.java
@@ -23,60 +23,57 @@ public abstract class ComponentListener<E extends ComponentInteractionEvent, C e
         this.componentClass = Reflection.getSuperClass(this, 2);
     }
 
+    /**
+     * Handle paging and component interaction.
+     * <br><br>
+     * Override for specific components.
+     *
+     * @param event Discord4J instance of ComponentInteractionEvent.
+     * @param responseCacheEntry Matched response cache entry.
+     */
+    protected Mono<Void> handleEvent(E event, DiscordResponseCache.Entry responseCacheEntry) {
+        return Flux.fromIterable(responseCacheEntry.getResponse().getPageComponents()) // Handle Top Level Paging Components
+            .flatMap(layoutComponent -> Flux.fromIterable(layoutComponent.getComponents()))
+            .filter(this.componentClass::isInstance) // Validate Component Type
+            .map(this.componentClass::cast)
+            .filter(component -> event.getCustomId().equals(component.getIdentifier())) // Validate Component ID
+            .singleOrEmpty()
+            .flatMap(component -> this.handlePagingInteraction(event, responseCacheEntry, component)) // Handle Response Paging
+            .switchIfEmpty(
+                Flux.fromIterable(responseCacheEntry.getResponse().getCurrentPage().getPageComponents()) // Handle Current Page SubPage Paging
+                    .flatMap(layoutComponent -> Flux.fromIterable(layoutComponent.getComponents()))
+                    .filter(this.componentClass::isInstance) // Validate Component Type
+                    .map(this.componentClass::cast)
+                    .filter(component -> event.getCustomId().equals(component.getIdentifier()))
+                    .singleOrEmpty()
+                    .flatMap(component -> this.handlePagingInteraction(event, responseCacheEntry, component))
+                    .switchIfEmpty(
+                        Flux.fromIterable(responseCacheEntry.getResponse().getCurrentPage().getComponents()) // Handle Component Interaction
+                            .flatMap(layoutComponent -> Flux.fromIterable(layoutComponent.getComponents()))
+                            .filter(this.componentClass::isInstance) // Validate Component Type
+                            .map(this.componentClass::cast)
+                            .filter(component -> event.getCustomId().equals(component.getIdentifier()))
+                            .singleOrEmpty()
+                            .flatMap(component -> this.handleInteraction(event, responseCacheEntry, component))
+                    )
+            );
+    }
+
     @Override
-    public Publisher<Void> apply(E componentEvent) {
+    public final Publisher<Void> apply(E componentEvent) {
         return Mono.just(componentEvent).flatMap(event -> Flux.fromIterable(this.getDiscordBot().getResponseCache())
             .filter(responseCacheEntry -> !event.getInteraction().getUser().isBot()) // Ignore Bots
             .filter(responseCacheEntry -> responseCacheEntry.getMessageId().equals(event.getMessageId())) // Validate Message ID
             .filter(responseCacheEntry -> responseCacheEntry.getUserId().equals(event.getInteraction().getUser().getId())) // Validate User ID
             .switchIfEmpty(event.deferEdit().then(Mono.empty())) // Invalid User Interaction
-            .flatMap(responseCacheEntry -> Flux.fromIterable(responseCacheEntry.getResponse().getPageComponents()) // Handle Top Level Paging Components
-                .flatMap(layoutComponent -> Flux.fromIterable(layoutComponent.getComponents()))
-                .filter(this.componentClass::isInstance) // Validate Component Type
-                .map(this.componentClass::cast)
-                .filter(component -> event.getCustomId().equals(component.getIdentifier())) // Validate Component ID
-                .singleOrEmpty()
-                .flatMap(component -> this.handlePagingInteraction(event, responseCacheEntry, component)) // Handle Response Paging
-                .switchIfEmpty(
-                    Flux.fromIterable(responseCacheEntry.getResponse().getCurrentPage().getPageComponents()) // Handle Current Page SubPage Paging
-                        .flatMap(layoutComponent -> Flux.fromIterable(layoutComponent.getComponents()))
-                        .filter(this.componentClass::isInstance) // Validate Component Type
-                        .map(this.componentClass::cast)
-                        .filter(component -> event.getCustomId().equals(component.getIdentifier()))
-                        .singleOrEmpty()
-                        .flatMap(component -> this.handlePagingInteraction(event, responseCacheEntry, component))
-                        .switchIfEmpty(
-                            Mono.justOrEmpty(responseCacheEntry.getActiveModal()) // Handle Active Modal
-                                .filter(modal -> event.getCustomId().equals(modal.getIdentifier()))
-                                .filter(this.componentClass::isInstance)
-                                .map(this.componentClass::cast)
-                                .flatMap(modal -> this.handleInteraction(event, responseCacheEntry, modal))
-                                .switchIfEmpty(
-                                    Flux.fromIterable(responseCacheEntry.getResponse().getCurrentPage().getComponents()) // Handle Component Interaction
-                                        .flatMap(layoutComponent -> Flux.fromIterable(layoutComponent.getComponents()))
-                                        .filter(this.componentClass::isInstance) // Validate Component Type
-                                        .map(this.componentClass::cast)
-                                        .filter(component -> event.getCustomId().equals(component.getIdentifier()))
-                                        .singleOrEmpty()
-                                        .flatMap(component -> this.handleInteraction(event, responseCacheEntry, component))
-                                        .switchIfEmpty(
-                                            Mono.just(responseCacheEntry.getResponse().getBackButton()) // Handle Back Button
-                                                .filter(this.componentClass::isInstance) // Validate Component Type
-                                                .map(this.componentClass::cast)
-                                                .filter(component -> event.getCustomId().equals(component.getIdentifier()))
-                                                .flatMap(component -> this.handlePagingInteraction(event, responseCacheEntry, component))
-                                        )
-                                )
-                        )
-                )
-            )
+            .flatMap(responseCacheEntry -> this.handleEvent(event, responseCacheEntry))
             .then()
         );
     }
 
     protected abstract C getContext(E event, Response cachedMessage, T component);
 
-    private Mono<Void> handleInteraction(E event, DiscordResponseCache.Entry responseCacheEntry, T component) {
+    protected final Mono<Void> handleInteraction(E event, DiscordResponseCache.Entry responseCacheEntry, T component) {
         return Mono.just(this.getContext(event, responseCacheEntry.getResponse(), component))
             .flatMap(context -> Mono.just(responseCacheEntry)
                 .onErrorResume(throwable -> this.getDiscordBot().handleException(
@@ -97,7 +94,7 @@ public abstract class ComponentListener<E extends ComponentInteractionEvent, C e
             );
     }
 
-    private Mono<Void> handlePagingInteraction(E event, DiscordResponseCache.Entry responseCacheEntry, T component) {
+    protected final Mono<Void> handlePagingInteraction(E event, DiscordResponseCache.Entry responseCacheEntry, T component) {
         return Mono.just(this.getContext(event, responseCacheEntry.getResponse(), component))
             .flatMap(context -> Mono.just(responseCacheEntry)
                 .onErrorResume(throwable -> this.getDiscordBot().handleException(
