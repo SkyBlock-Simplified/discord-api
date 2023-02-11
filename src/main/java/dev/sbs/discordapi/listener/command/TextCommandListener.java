25fc4e39ef035586f59c32c94e0220a9bc8d957c
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-02-10 22:07:30 -0500

    Fix text command detection

--- src/main/java/dev/sbs/discordapi/listener/command/TextCommandListener.java
+++ src/main/java/dev/sbs/discordapi/listener/command/TextCommandListener.java
@@ -5,8 +5,6 @@ import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.helper.ListUtil;
 import dev.sbs.api.util.helper.StringUtil;
 import dev.sbs.discordapi.DiscordBot;
-import dev.sbs.discordapi.command.Command;
-import dev.sbs.discordapi.command.ParentCommand;
 import dev.sbs.discordapi.command.data.Argument;
 import dev.sbs.discordapi.command.data.Parameter;
 import dev.sbs.discordapi.context.message.text.TextCommandContext;
@@ -16,8 +14,6 @@ import discord4j.core.object.entity.User;
 import org.jetbrains.annotations.NotNull;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
-
-import java.util.Optional;
 
 public class TextCommandListener extends DiscordListener<MessageCreateEvent> {
 
@@ -34,26 +30,24 @@ public class TextCommandListener extends DiscordListener<MessageCreateEvent> {
                 String message = StringUtil.defaultIfEmpty(event.getMessage().getContent(), "");
                 String[] messageArguments = message.split(" ");
 
-                return Mono.justOrEmpty(this.getDiscordBot().getCommandRegistrar().getPrefixCommand())
-                    .filter(prefixCommand -> prefixCommand == null || this.getCommandAnnotation(prefixCommand).filter(command -> this.doesCommandMatch(command, messageArguments[0])).isPresent())
+                return Mono.justOrEmpty(this.getDiscordBot().getCommandRegistrar().getRootCommandRelationship())
+                    .filter(rootRelationship -> this.doesCommandMatch(rootRelationship, messageArguments[0]))
                     .flatMap(prefixCommand -> Mono.justOrEmpty(this.getDeepestCommand(messageArguments)))
-                    .filter(relationship -> !relationship.getCommandClass().isAssignableFrom(ParentCommand.class))
                     .flatMap(relationship -> {
                         ConcurrentList<String> remainingArguments = Concurrent.newList(messageArguments);
 
                         // Trim Parent Commands
-                        this.getParentCommandList(relationship.getCommandClass())
+                        relationship.getInstance()
+                            .getParentCommands()
                             .stream()
-                            .map(Command.RelationshipData::getOptionalCommandInfo)
-                            .flatMap(Optional::stream)
-                            .filter(parentCommandInfo -> this.doesCommandMatch(parentCommandInfo, remainingArguments.get(0)))
+                            .filter(parentRelationship -> this.doesCommandMatch(parentRelationship, remainingArguments.get(0)))
                             .forEach(__ -> remainingArguments.remove(0));
 
                         // Store Used Alias
                         String commandAlias = remainingArguments.get(0);
 
                         // Trim Command
-                        if (this.doesCommandMatch(relationship.getCommandInfo(), remainingArguments.get(0)))
+                        if (this.doesCommandMatch(relationship, remainingArguments.get(0)))
                             remainingArguments.remove(0);
 
                         // Build Arguments
