679275679dc1ec6c38867747e5f484d739267f03
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-05-25 01:35:31 -0400

    Annotate all method parameters as NotNull

--- src/main/java/dev/sbs/discordapi/listener/message/component/SelectMenuListener.java
+++ src/main/java/dev/sbs/discordapi/listener/message/component/SelectMenuListener.java
@@ -5,21 +5,22 @@ import dev.sbs.discordapi.context.interaction.deferrable.component.action.select
 import dev.sbs.discordapi.response.Response;
 import dev.sbs.discordapi.response.component.interaction.action.SelectMenu;
 import discord4j.core.event.domain.interaction.SelectMenuInteractionEvent;
+import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
 public final class SelectMenuListener extends ComponentListener<SelectMenuInteractionEvent, SelectMenuContext, SelectMenu> {
 
-    public SelectMenuListener(DiscordBot discordBot) {
+    public SelectMenuListener(@NotNull DiscordBot discordBot) {
         super(discordBot);
     }
 
     @Override
-    protected SelectMenuContext getContext(SelectMenuInteractionEvent event, Response response, SelectMenu component) {
+    protected SelectMenuContext getContext(@NotNull SelectMenuInteractionEvent event, @NotNull Response response, @NotNull SelectMenu component) {
         return SelectMenuContext.of(this.getDiscordBot(), event, response, component);
     }
 
     @Override
-    protected Mono<Void> handlePaging(SelectMenuContext selectMenuContext) {
+    protected Mono<Void> handlePaging(@NotNull SelectMenuContext selectMenuContext) {
         return Mono.just(selectMenuContext)
             .doOnNext(context -> context.getComponent().updateSelected(context.getEvent().getValues()))
             .flatMap(context -> Mono.justOrEmpty(context.getResponse())
@@ -27,12 +28,24 @@ public final class SelectMenuListener extends ComponentListener<SelectMenuIntera
                     String selectedValue = context.getSelected().getFirst().orElseThrow().getValue();
 
                     switch (context.getComponent().getPageType()) {
-                        case PAGE -> response.getHandler().gotoPage(selectedValue);
+                        case PAGE -> response.getHistoryHandler().gotoPage(selectedValue);
                         case SUBPAGE -> {
                             if (selectedValue.equals("BACK"))
-                                response.getHandler().gotoPreviousPage();
+                                response.getHistoryHandler().gotoPreviousPage();
                             else
-                                response.getHandler().gotoSubPage(selectedValue);
+                                response.getHistoryHandler().gotoSubPage(selectedValue);
+                        }
+                        case ITEM -> {
+                            /*
+                             TODO
+                                Build a viewer that converts the item list
+                                into something that either lists data about an item
+                                or a sub-list from PageItem
+
+                             TODO
+                                Viewer will need the ability to enable editing
+                                and code to do something on save
+                             */
                         }
                     }
 
