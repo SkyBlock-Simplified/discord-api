72c8af07045204c42a73f6618da780ce20aa4111
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-05-25 01:23:45 -0400

    Instantly edit built-in interaction deferReply

--- src/main/java/dev/sbs/discordapi/context/interaction/deferrable/DeferrableInteractionContext.java
+++ src/main/java/dev/sbs/discordapi/context/interaction/deferrable/DeferrableInteractionContext.java
@@ -5,23 +5,25 @@ import dev.sbs.discordapi.response.Response;
 import discord4j.core.event.domain.interaction.DeferrableInteractionEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.spec.InteractionCallbackSpec;
+import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 public interface DeferrableInteractionContext<T extends DeferrableInteractionEvent> extends InteractionContext<T> {
 
     @Override
-    default Mono<Message> buildMessage(Response response) {
+    default Mono<Message> buildMessage(@NotNull Response response) {
         return this.getEvent()
-            .editReply(response.getD4jInteractionEditSpec())
+            .editReply(response.getD4jInteractionReplyEditSpec())
             .publishOn(response.getReactorScheduler());
     }
 
-    default Mono<Void> deferReply() {
-        return this.deferReply(false);
-    }
-
-    default Mono<Void> deferReply(boolean ephemeral) {
-        return this.getEvent().deferReply(InteractionCallbackSpec.builder().ephemeral(ephemeral).build());
+    @Override
+    default Mono<Void> deferReply(boolean ephemeral, @NotNull Optional<String> content) {
+        return this.getEvent()
+            .deferReply(InteractionCallbackSpec.builder().ephemeral(ephemeral).build())
+            .then(this.edit(content));
     }
 
 }
