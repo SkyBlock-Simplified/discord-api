e547bf229afbee9ffcd3912b9dc1aed342396e1f
Nahydrin <6407029+CraftedFury@users.noreply.github.com>
2023-08-24 22:29:36 -0400

    Move database dump to discord-api tests

---
+++ src/test/java/dev/sbs/discordapi/SqlToJsonDumpTest.java
@@ -0,0 +1,61 @@
+package dev.sbs.discordapi;
+
+import dev.sbs.api.SimplifiedApi;
+import dev.sbs.api.data.DataSession;
+import dev.sbs.api.data.model.Model;
+import dev.sbs.api.util.data.tuple.Pair;
+import dev.sbs.api.util.helper.DataUtil;
+import dev.sbs.api.util.helper.SystemUtil;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Table;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SqlToJsonDumpTest {
+
+    private static final TestConfig testConfig;
+
+    static {
+        try {
+            File currentDir = new File(SimplifiedApi.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            testConfig = new TestConfig(currentDir.getParentFile(), "testsql");
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("Unable to retrieve current directory", exception); // Should never get here
+        }
+    }
+
+    @Test
+    public void dumpDatabaseToJson_ok() {
+        File currentDir = SystemUtil.getUserDir();
+        File dbDir = new File(currentDir, "build/db");
+
+        if (!dbDir.exists())
+            dbDir.mkdirs();
+
+        System.out.println("Connecting to database...");
+        SimplifiedApi.connectSession(DataSession.Type.SQL, testConfig);
+
+        SimplifiedApi.getSession()
+            .getModels()
+            .stream()
+            .map(modelClass -> (Class<? extends Model>) modelClass)
+            .filter(modelClass -> modelClass.isAnnotationPresent(Table.class))
+            .map(modelClass -> Pair.of(
+                modelClass,
+                modelClass.getAnnotation(Table.class).name()
+            ))
+            .forEach(entry -> {
+                String tableName = entry.getValue();
+                System.out.println("Saving " + tableName + "...");
+
+                try (FileWriter fileWriter = DataUtil.newFileWriter(dbDir.getAbsolutePath() + "/" + tableName + ".json")) {
+                    fileWriter.write(SimplifiedApi.getGson().toJson(SimplifiedApi.getRepositoryOf(entry.getKey()).findAll()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+    }
+
+}
---
+++ src/test/java/dev/sbs/discordapi/TestConfig.java
@@ -0,0 +1,26 @@
+package dev.sbs.discordapi;
+
+import dev.sbs.api.SimplifiedApi;
+import dev.sbs.api.data.sql.SqlConfig;
+import dev.sbs.api.util.helper.ResourceUtil;
+import dev.sbs.api.util.helper.StringUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.UUID;
+
+public class TestConfig extends SqlConfig {
+
+    private final Optional<UUID> hypixelApiKey = ResourceUtil.getEnv("HYPIXEL_API_KEY").map(StringUtil::toUUID);
+
+    public TestConfig(@NotNull File configDir, @NotNull String fileName, String... header) {
+        super(configDir, fileName, header);
+        this.hypixelApiKey.ifPresent(value -> SimplifiedApi.getKeyManager().add("HYPIXEL_API_KEY", value));
+    }
+
+    public Optional<UUID> getHypixelApiKey() {
+        return this.hypixelApiKey;
+    }
+
+}
