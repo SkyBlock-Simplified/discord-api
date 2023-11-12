b47e43be3a604f019c144569c1d32249ccd9024c
Nahydrin <6407029+CraftedFury@users.noreply.github.com>
2023-11-11 19:50:54 -0500

    Renamed

--- src/main/java/dev/sbs/discordapi/command/exception/CommandException.java
+++ src/main/java/dev/sbs/discordapi/command/exception/CommandException.java
@@ -3,12 +3,12 @@ package dev.sbs.discordapi.command.exception;
 import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.collection.concurrent.ConcurrentMap;
 import dev.sbs.api.util.data.tuple.Triple;
-import dev.sbs.discordapi.command.Command;
+import dev.sbs.discordapi.command.impl.SlashCommand;
 import dev.sbs.discordapi.util.exception.DiscordException;
 
 /**
  * {@link CommandException CommandExceptions} are thrown when any class extending the
- * {@link Command} class is unable to complete.
+ * {@link SlashCommand} class is unable to complete.
  */
 public class CommandException extends DiscordException {
 
