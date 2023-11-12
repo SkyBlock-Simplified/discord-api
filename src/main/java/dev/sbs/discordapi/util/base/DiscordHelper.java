5bdd0823c42adc943c594bab5a843d5dca546332
Nahydrin <6407029+CraftedFury@users.noreply.github.com>
2023-11-11 22:58:12 -0500

    Lombok logger

--- src/main/java/dev/sbs/discordapi/util/base/DiscordHelper.java
+++ src/main/java/dev/sbs/discordapi/util/base/DiscordHelper.java
@@ -2,19 +2,22 @@ package dev.sbs.discordapi.util.base;
 
 import dev.sbs.discordapi.DiscordBot;
 import lombok.Getter;
-import org.apache.logging.log4j.LogManager;
+import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
+@Log4j2
 public abstract class DiscordHelper extends DiscordReference {
 
     private final @NotNull DiscordBot discordBot;
-    private final @NotNull Logger log;
 
     protected DiscordHelper(@NotNull DiscordBot discordBot) {
         this.discordBot = discordBot;
-        this.log = LogManager.getLogger(this);
+    }
+
+    public final @NotNull Logger getLog() {
+        return log;
     }
 
 }
