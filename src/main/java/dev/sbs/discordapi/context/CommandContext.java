ee711e5914d2f769d2858521a4d29609743d098b
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-05 13:19:00 -0500

    Only support deferrable commands

--- src/main/java/dev/sbs/discordapi/context/CommandContext.java
+++ src/main/java/dev/sbs/discordapi/context/CommandContext.java
@@ -4,12 +4,13 @@ import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.discordapi.command.parameter.Argument;
 import dev.sbs.discordapi.command.parameter.Parameter;
 import dev.sbs.discordapi.context.interaction.TypingContext;
-import discord4j.core.event.domain.Event;
+import dev.sbs.discordapi.context.interaction.deferrable.DeferrableInteractionContext;
+import discord4j.core.event.domain.interaction.DeferrableInteractionEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
-public interface CommandContext<T extends Event> extends TypingContext<T> {
+public interface CommandContext<T extends DeferrableInteractionEvent> extends DeferrableInteractionContext<T>, TypingContext<T> {
 
     /**
      * Finds the argument for a known {@link Parameter}.
