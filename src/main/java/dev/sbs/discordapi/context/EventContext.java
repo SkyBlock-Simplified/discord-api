5aff54d68488dc240298a0bd3d455c0c26924f19
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-05 13:15:54 -0500

    Remove deferReply from EventContext, remove Response#loader

--- src/main/java/dev/sbs/discordapi/context/EventContext.java
+++ src/main/java/dev/sbs/discordapi/context/EventContext.java
@@ -1,10 +1,8 @@
 package dev.sbs.discordapi.context;
 
-import dev.sbs.api.util.helper.StringUtil;
 import dev.sbs.discordapi.DiscordBot;
 import dev.sbs.discordapi.context.exception.ExceptionContext;
 import dev.sbs.discordapi.response.Response;
-import dev.sbs.discordapi.util.cache.ResponseCache;
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.Event;
 import discord4j.core.object.entity.Guild;
@@ -12,9 +10,7 @@ import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.entity.channel.PrivateChannel;
-import org.intellij.lang.annotations.PrintFormat;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,22 +19,6 @@ import java.util.UUID;
 import java.util.function.Function;
 
 public interface EventContext<T extends Event> {
-
-    default Mono<Void> deferReply() {
-        return this.deferReply(false);
-    }
-
-    default Mono<Void> deferReply(boolean ephemeral) {
-        return this.deferReply(ephemeral, Optional.empty());
-    }
-
-    default Mono<Void> deferReply(boolean ephemeral, @PrintFormat @Nullable String content, @Nullable Object... args) {
-        return this.deferReply(ephemeral, StringUtil.formatNullable(content, args));
-    }
-
-    default Mono<Void> deferReply(boolean ephemeral, @NotNull Optional<String> content) {
-        return Mono.defer(() -> this.reply(Response.loader(this, ephemeral, content)));
-    }
 
     default Mono<Message> discordBuildMessage(@NotNull Response response) {
         return this.getChannel()
@@ -57,14 +37,6 @@ public interface EventContext<T extends Event> {
         return this.getChannel()
             .flatMap(channel -> channel.getMessageById(messageId))
             .flatMap(message -> message.edit(response.getD4jEditSpec()));
-    }
-
-    default Mono<Void> edit(@PrintFormat @Nullable String content, @Nullable Object... args) {
-        return this.edit(StringUtil.formatNullable(content, args));
-    }
-
-    default Mono<Void> edit(@NotNull Optional<String> content) {
-        return this.edit(Response.loader(this, false, content));
     }
 
     default Mono<Void> edit(@NotNull Response response) {
@@ -119,7 +91,7 @@ public interface EventContext<T extends Event> {
     }
 
     default Mono<Void> reply(@NotNull Response response) {
-        return Flux.fromIterable(this.getDiscordBot().getResponseCache())
+        return this.discordBuildMessage(response)
             .checkpoint("EventContext#reply Processing")
             .onErrorResume(throwable -> this.getDiscordBot().handleException(
                 ExceptionContext.of(
@@ -129,36 +101,21 @@ public interface EventContext<T extends Event> {
                     "Event Reply Exception"
                 )
             ))
-            .filter(entry -> entry.getResponse().getUniqueId().equals(this.getResponseId())) // Search For Loader
-            .filter(ResponseCache.Entry::isLoading)
-            .singleOrEmpty()
-            .flatMap(ResponseCache.Entry::setLoaded)
-            .flatMap(entry -> this.discordEditMessage(entry.getMessageId(), response) // Edit Message Directly
-                .flatMap(message -> entry.updateResponse(response)
-                    .then(entry.updateReactions(message))
+            .flatMap(message -> this.getDiscordBot()
+                .getResponseCache()
+                .createAndGet(
+                    message.getChannelId(),
+                    this.getInteractUserId(),
+                    message.getId(),
+                    response
+                )
+                .updateResponse(response)
+                .flatMap(entry -> entry.updateReactions(message)
                     .then(entry.updateAttachments(message))
                     .then(entry.updateLastInteract())
                 )
-                .thenReturn(entry)
-            )
-            .switchIfEmpty(
-                this.discordBuildMessage(response) // Create New Message
-                    .flatMap(message -> this.getDiscordBot()
-                        .getResponseCache()
-                        .createAndGet(
-                            message.getChannelId(),
-                            this.getInteractUserId(),
-                            message.getId(),
-                            response
-                        )
-                        .updateResponse(response)
-                        .flatMap(entry -> entry.updateReactions(message)
-                            .then(entry.updateAttachments(message))
-                            .then(entry.updateLastInteract())
-                        )
-                    )
-            )
-            .then();
+                .then()
+            );
     }
 
     default Mono<Void> withChannel(Function<MessageChannel, Mono<Void>> messageChannelFunction) {
