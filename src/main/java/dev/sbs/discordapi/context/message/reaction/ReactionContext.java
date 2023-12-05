b0550efb17976dd399b276565f4b3c4b9de30a92
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2023-12-05 13:23:24 -0500

    Merge implementations into ReactionContext

--- src/main/java/dev/sbs/discordapi/context/message/reaction/ReactionContext.java
+++ src/main/java/dev/sbs/discordapi/context/message/reaction/ReactionContext.java
@@ -5,14 +5,22 @@ import dev.sbs.discordapi.context.ResponseContext;
 import dev.sbs.discordapi.response.Emoji;
 import dev.sbs.discordapi.response.Response;
 import dev.sbs.discordapi.util.cache.ResponseCache;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.MessageEvent;
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.event.domain.message.ReactionRemoveEvent;
+import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ReactionContext extends ResponseContext<MessageEvent> {
 
@@ -110,11 +118,119 @@ public interface ReactionContext extends ResponseContext<MessageEvent> {
     }
 
     static ReactionContext ofAdd(@NotNull DiscordBot discordBot, @NotNull ReactionAddEvent event, @NotNull Response cachedMessage, @NotNull Emoji emoji, @NotNull Optional<ResponseCache.Followup> followup) {
-        return new ReactionAddContextImpl(discordBot, event, cachedMessage.getUniqueId(), emoji, followup);
+        return new AddImpl(
+            discordBot,
+            event,
+            cachedMessage.getUniqueId(),
+            emoji,
+            followup
+        );
     }
 
     static ReactionContext ofRemove(@NotNull DiscordBot discordBot, @NotNull ReactionRemoveEvent event, @NotNull Response cachedMessage, @NotNull Emoji emoji, @NotNull Optional<ResponseCache.Followup> followup) {
-        return new ReactionRemoveContextImpl(discordBot, event, cachedMessage.getUniqueId(), emoji, followup);
+        return new RemoveImpl(
+            discordBot,
+            event,
+            cachedMessage.getUniqueId(),
+            emoji,
+            followup
+        );
+    }
+
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+    class AddImpl implements ReactionContext {
+
+        private final @NotNull DiscordBot discordBot;
+        private final @NotNull ReactionAddEvent event;
+        private final @NotNull UUID responseId;
+        private final @NotNull Emoji emoji;
+        private final @NotNull Type type = Type.ADD;
+        private final @NotNull Optional<ResponseCache.Followup> followup;
+
+        @Override
+        public @NotNull Snowflake getChannelId() {
+            return this.getEvent().getChannelId();
+        }
+
+        @Override
+        public Mono<Guild> getGuild() {
+            return this.getEvent().getGuild();
+        }
+
+        @Override
+        public Optional<Snowflake> getGuildId() {
+            return this.getEvent().getGuildId();
+        }
+
+        @Override
+        public @NotNull User getInteractUser() {
+            return Objects.requireNonNull(this.getEvent().getUser().block());
+        }
+
+        @Override
+        public @NotNull Snowflake getInteractUserId() {
+            return this.getEvent().getUserId();
+        }
+
+        @Override
+        public Mono<Message> getMessage() {
+            return this.getEvent().getMessage();
+        }
+
+        @Override
+        public Snowflake getMessageId() {
+            return this.getEvent().getMessageId();
+        }
+
+    }
+
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+    class RemoveImpl implements ReactionContext {
+
+        private final @NotNull DiscordBot discordBot;
+        private final @NotNull ReactionRemoveEvent event;
+        private final @NotNull UUID responseId;
+        private final @NotNull Emoji emoji;
+        private final @NotNull Type type = Type.REMOVE;
+        private final @NotNull Optional<ResponseCache.Followup> followup;
+
+        @Override
+        public @NotNull Snowflake getChannelId() {
+            return this.getEvent().getChannelId();
+        }
+
+        @Override
+        public Mono<Guild> getGuild() {
+            return this.getEvent().getGuild();
+        }
+
+        @Override
+        public Optional<Snowflake> getGuildId() {
+            return this.getEvent().getGuildId();
+        }
+
+        @Override
+        public @NotNull User getInteractUser() {
+            return Objects.requireNonNull(this.getEvent().getUser().block());
+        }
+
+        @Override
+        public @NotNull Snowflake getInteractUserId() {
+            return this.getEvent().getUserId();
+        }
+
+        @Override
+        public Mono<Message> getMessage() {
+            return this.getEvent().getMessage();
+        }
+
+        @Override
+        public Snowflake getMessageId() {
+            return this.getEvent().getMessageId();
+        }
+
     }
 
     enum Type {
