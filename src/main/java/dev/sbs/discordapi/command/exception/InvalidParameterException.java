1e21d8dff3814b1209abb8f544c7da8a0a0fd10c
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2024-01-09 18:57:49 -0500

    InvalidParameterException remains as the only possible parameter error

--- src/main/java/dev/sbs/discordapi/command/exception/InvalidParameterException.java
+++ src/main/java/dev/sbs/discordapi/command/exception/InvalidParameterException.java
@@ -1,4 +1,4 @@
-package dev.sbs.discordapi.command.exception.parameter;
+package dev.sbs.discordapi.command.exception;
 
 import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.collection.concurrent.ConcurrentMap;
@@ -7,7 +7,7 @@ import dev.sbs.api.util.mutable.triple.Triple;
 /**
  * {@link InvalidParameterException InvalidParameterExceptions} are thrown when the user passes invalid arguments to a command.
  */
-public final class InvalidParameterException extends ParameterException {
+public final class InvalidParameterException extends CommandException {
 
     private InvalidParameterException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ConcurrentList<Triple<String, String, Boolean>> fields, ConcurrentMap<String, Object> data) {
         super(message, cause, enableSuppression, writableStackTrace, fields, data);
