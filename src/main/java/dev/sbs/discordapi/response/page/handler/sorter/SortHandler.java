4ef2555086a3032708a0d9eb79deb8734ba56cb3
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-21 22:34:01 -0400

    Use new OutputHandler

--- src/main/java/dev/sbs/discordapi/response/page/handler/sorter/SortHandler.java
+++ src/main/java/dev/sbs/discordapi/response/page/handler/sorter/SortHandler.java
@@ -3,7 +3,7 @@ package dev.sbs.discordapi.response.page.handler.sorter;
 import dev.sbs.api.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.builder.hash.EqualsBuilder;
 import dev.sbs.api.util.builder.hash.HashCodeBuilder;
-import dev.sbs.discordapi.response.page.handler.cache.CacheHandler;
+import dev.sbs.discordapi.response.page.handler.OutputHandler;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
@@ -11,21 +11,20 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Optional;
 
 @Getter
-public class SortHandler<T> implements CacheHandler {
+public class SortHandler<T> implements OutputHandler<Sorter<T>> {
 
-    private final @NotNull ConcurrentList<Sorter<T>> sorters;
+    private final @NotNull ConcurrentList<Sorter<T>> items;
     private int currentSorterIndex = -1;
     private boolean reversed = false;
     @Setter private boolean cacheUpdateRequired;
 
-    public SortHandler(@NotNull ConcurrentList<Sorter<T>> sorters) {
-        this.sorters = sorters;
+    public SortHandler(@NotNull ConcurrentList<Sorter<T>> items) {
+        this.items = items;
         this.gotoNext();
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         SortHandler<?> that = (SortHandler<?>) o;
@@ -34,37 +33,33 @@ public class SortHandler<T> implements CacheHandler {
             .append(this.getCurrentSorterIndex(), that.getCurrentSorterIndex())
             .append(this.isReversed(), that.isReversed())
             .append(this.isCacheUpdateRequired(), that.isCacheUpdateRequired())
-            .append(this.getSorters(), that.getSorters())
+            .append(this.getItems(), that.getItems())
             .build();
-    }
-
-    public @NotNull Optional<Sorter<T>> getCurrent() {
-        return Optional.ofNullable(this.getCurrentSorterIndex() > -1 ? this.getSorters().get(this.getCurrentSorterIndex()) : null);
-    }
-
-    public void gotoNext() {
-        if (this.hasSorters()) {
-            this.currentSorterIndex++;
-
-            if (this.currentSorterIndex >= this.getSorters().size())
-                this.currentSorterIndex = 0;
-
-            this.setCacheUpdateRequired();
-        }
     }
 
     @Override
     public int hashCode() {
         return new HashCodeBuilder()
-            .append(this.getSorters())
+            .append(this.getItems())
             .append(this.getCurrentSorterIndex())
             .append(this.isReversed())
             .append(this.isCacheUpdateRequired())
             .build();
     }
 
-    public boolean hasSorters() {
-        return this.getSorters().notEmpty();
+    public @NotNull Optional<Sorter<T>> getCurrent() {
+        return Optional.ofNullable(this.getCurrentSorterIndex() > -1 ? this.getItems().get(this.getCurrentSorterIndex()) : null);
+    }
+
+    public void gotoNext() {
+        if (this.notEmpty()) {
+            this.currentSorterIndex++;
+
+            if (this.currentSorterIndex >= this.getItems().size())
+                this.currentSorterIndex = 0;
+
+            this.setCacheUpdateRequired();
+        }
     }
 
     public void invertOrder() {
