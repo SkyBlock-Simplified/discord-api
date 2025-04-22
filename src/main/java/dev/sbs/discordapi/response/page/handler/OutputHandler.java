cf6135889853f5495cdd2170e2daf367ea0062fd
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-21 22:33:23 -0400

    Rename CacheHandler to OutputHandler, add more baseline methods

---
+++ src/main/java/dev/sbs/discordapi/response/page/handler/OutputHandler.java
@@ -0,0 +1,26 @@
+package dev.sbs.discordapi.response.page.handler;
+
+import dev.sbs.api.collection.concurrent.ConcurrentList;
+import org.jetbrains.annotations.NotNull;
+
+public interface OutputHandler<T> {
+
+    @NotNull ConcurrentList<T> getItems();
+
+    boolean isCacheUpdateRequired();
+
+    default boolean isEmpty() {
+        return this.getItems().isEmpty();
+    }
+
+    default boolean notEmpty() {
+        return this.getItems().notEmpty();
+    }
+
+    default void setCacheUpdateRequired() {
+        this.setCacheUpdateRequired(true);
+    }
+
+    void setCacheUpdateRequired(boolean cacheUpdateRequired);
+
+}
