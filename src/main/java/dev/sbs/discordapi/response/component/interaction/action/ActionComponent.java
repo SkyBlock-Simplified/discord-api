a2fc5c5ca3a9990b3f9f0c54324096edead758fd
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-30 17:00:46 -0400

    Use updated interfaces

--- src/main/java/dev/sbs/discordapi/response/component/interaction/action/ActionComponent.java
+++ src/main/java/dev/sbs/discordapi/response/component/interaction/action/ActionComponent.java
@@ -1,11 +1,10 @@
 package dev.sbs.discordapi.response.component.interaction.action;
 
 import dev.sbs.discordapi.response.component.Component;
-import dev.sbs.discordapi.response.component.type.D4jComponent;
-import dev.sbs.discordapi.response.component.type.IdentifiableComponent;
+import dev.sbs.discordapi.response.component.type.UserInteractComponent;
 import org.jetbrains.annotations.NotNull;
 
-public interface ActionComponent extends Component, IdentifiableComponent, D4jComponent {
+public interface ActionComponent extends Component, UserInteractComponent {
 
     @NotNull discord4j.core.object.component.ActionComponent getD4jComponent();
 
