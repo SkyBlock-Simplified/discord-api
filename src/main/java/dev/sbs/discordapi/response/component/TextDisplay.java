ebcd1c42eb5a76d5183712b2117fc5ef6e42269c
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-30 16:41:35 -0400

    Add TextDisplay component

---
+++ src/main/java/dev/sbs/discordapi/response/component/TextDisplay.java
@@ -0,0 +1,66 @@
+package dev.sbs.discordapi.response.component;
+
+import dev.sbs.api.util.builder.hash.EqualsBuilder;
+import dev.sbs.api.util.builder.hash.HashCodeBuilder;
+import dev.sbs.discordapi.response.component.type.MessageComponent;
+import dev.sbs.discordapi.response.component.type.TopLevelMessageComponent;
+import dev.sbs.discordapi.response.component.type.v2.ContainerComponent;
+import dev.sbs.discordapi.response.component.type.v2.SectionComponent;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.intellij.lang.annotations.PrintFormat;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TextDisplay implements MessageComponent, TopLevelMessageComponent, ContainerComponent, SectionComponent {
+
+    private final @NotNull String identifier = UUID.randomUUID().toString();
+    private final @NotNull String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TextDisplay textDisplay = (TextDisplay) o;
+
+        return new EqualsBuilder()
+            .append(this.getIdentifier(), textDisplay.getIdentifier())
+            .append(this.getContent(), textDisplay.getContent())
+            .build();
+    }
+
+    @Override
+    public @NotNull Type getType() {
+        return Type.TEXT_DISPLAY;
+    }
+
+    public static @NotNull TextDisplay of(@NotNull String content) {
+        return new TextDisplay(content);
+    }
+
+    public static @NotNull TextDisplay of(@NotNull @PrintFormat String content, @Nullable Object... args) {
+        return new TextDisplay(String.format(content, args));
+    }
+
+    @Override
+    public @NotNull discord4j.core.object.component.TextDisplay getD4jComponent() {
+        return discord4j.core.object.component.TextDisplay.of(
+            this.getContent()
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(this.getIdentifier())
+            .append(this.getContent())
+            .build();
+    }
+
+}
