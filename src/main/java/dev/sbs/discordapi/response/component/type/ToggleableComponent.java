da2878c5e6fa99b5c2715f42102387f9e6a18fba
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-30 16:56:42 -0400

    Allow toggleable components to be generically state-changed

--- src/main/java/dev/sbs/discordapi/response/component/type/ToggleableComponent.java
+++ src/main/java/dev/sbs/discordapi/response/component/type/ToggleableComponent.java
@@ -1,8 +1,9 @@
 package dev.sbs.discordapi.response.component.type;
 
+import dev.sbs.discordapi.response.component.Component;
 import org.jetbrains.annotations.NotNull;
 
-public interface ToggleableComponent extends MessageComponent {
+public interface ToggleableComponent extends Component {
 
     @NotNull ToggleableComponent setState(boolean enabled);
 
