218f7587da8a4f7311b9318e384ce208de6c0150
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-30 17:00:16 -0400

    All components now provide their D4J equivalent, an identifier and their type

--- src/main/java/dev/sbs/discordapi/response/component/Component.java
+++ src/main/java/dev/sbs/discordapi/response/component/Component.java
@@ -1,5 +1,71 @@
 package dev.sbs.discordapi.response.component;
 
+import discord4j.core.object.entity.Message;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
 public interface Component {
+
+    @NotNull discord4j.core.object.component.MessageComponent getD4jComponent();
+
+    @NotNull String getIdentifier();
+
+    @NotNull Type getType();
+
+    @Getter
+    @RequiredArgsConstructor
+    enum Type {
+
+        UNKNOWN(-1),
+        ACTION_ROW(1),
+        BUTTON(2),
+        SELECT_MENU(3),
+        TEXT_INPUT(4),
+        SELECT_MENU_USER(5),
+        SELECT_MENU_ROLE(6),
+        SELECT_MENU_MENTIONABLE(7),
+        SELECT_MENU_CHANNEL(8),
+        SECTION(9, true),
+        TEXT_DISPLAY(10, true),
+        THUMBNAIL(11, true),
+        MEDIA_GALLERY(12, true),
+        FILE(13, true),
+        SEPARATOR(14, true),
+        CONTAINER(17, true);
+
+        private final int value;
+
+        /**
+         * Gets if this Type requires the use of {@link Message.Flag#IS_COMPONENTS_V2}.
+         */
+        private final boolean requireFlag;
+
+        Type(int value) {
+            this(value, false);
+        }
+
+        public static @NotNull Type of(int value) {
+            return switch (value) {
+                case 1 -> ACTION_ROW;
+                case 2 -> BUTTON;
+                case 3 -> SELECT_MENU;
+                case 4 -> TEXT_INPUT;
+                case 5 -> SELECT_MENU_USER;
+                case 6 -> SELECT_MENU_ROLE;
+                case 7 -> SELECT_MENU_MENTIONABLE;
+                case 8 -> SELECT_MENU_CHANNEL;
+                case 9 -> SECTION;
+                case 10 -> TEXT_DISPLAY;
+                case 11 -> THUMBNAIL;
+                case 12 -> MEDIA_GALLERY;
+                case 13 -> FILE;
+                case 14 -> SEPARATOR;
+                case 17 -> CONTAINER;
+                default -> UNKNOWN;
+            };
+        }
+
+    }
 
 }
