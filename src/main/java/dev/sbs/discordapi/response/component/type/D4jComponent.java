93ce5b0d8d316c5f23b24c338a0e81a3dad92b70
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-04-30 16:37:45 -0400

    These interfaces are no longer used

--- src/main/java/dev/sbs/discordapi/response/component/type/D4jComponent.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.discordapi.response.component.type;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface D4jComponent {
-
-    @NotNull discord4j.core.object.component.MessageComponent getD4jComponent();
-
-}
--- src/main/java/dev/sbs/discordapi/response/component/type/IdentifiableComponent.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.discordapi.response.component.type;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface IdentifiableComponent {
-
-    @NotNull String getIdentifier();
-
-}
